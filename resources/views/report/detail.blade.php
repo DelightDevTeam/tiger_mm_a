6ec57e648912305aeea969f1b384a6167ac8356c
sophia <thangneimawi@hivelocity.co.jp>
2024-11-18 22:13:00 +0700

    fixed title

--- resources/views/report/detail.blade.php
+++ resources/views/report/detail.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Lucky M</title>
+    <title>TigerMM Slot</title>
     <script src="{{asset('admin_app/assets/js/jquery.min.js')}}"></script>
     <script src="{{asset('admin_app/assets/js/jquery.datatable.min.js')}}"></script>
     <script
