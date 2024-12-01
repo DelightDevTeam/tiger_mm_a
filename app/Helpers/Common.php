3414af86891ae9b7ec0c79c611a23168685df959
sophia <thangneimawi@hivelocity.co.jp>
2024-12-01 11:05:25 +0700

    fixed noti

--- app/Helpers/Common.php
+++ app/Helpers/Common.php
@@ -4,13 +4,14 @@ namespace App\Helpers;
 
 use App\Models\DepositRequest;
 use App\Models\WithDrawRequest;
+use Illuminate\Support\Facades\Auth;
 
 class Common
 {
     public static function getNotiCount()
     {
-        $withDrawCount = WithDrawRequest::where('status', 'pending')->count();
-        $depositCount = DepositRequest::where('status', 'pending')->count();
+        $withDrawCount = WithDrawRequest::where('status', 'pending')->where('agent_id', Auth::id())->count();
+        $depositCount = DepositRequest::where('status', 'pending')->where('agent_id', Auth::id())->count();
         
         return $withDrawCount + $depositCount;
     }
